fc52b1424ec988de28ddea29a0390d20fe53d79e
Michael Ferlis <michaelferlis@gmail.com>
2019-05-15 08:57:56 -0500

    bonus calc started

--- client.js
+++ client.js
@@ -33,6 +33,61 @@ const employees = [
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
+for (let i = 0; i < employees.length; i++) {
+  employeeName(employees[i]);
+}
+
+function employeeName(person) {
+  console.log(person.name);
+}
+
+for (let i = 0; i < employees.length; i++) {
+  numberPerson(employees[i]);
+}
+
+function numberPerson(person) {
+
+  console.log(person.employeeNumber);
+}
+
+for (let i = 0; i < employees.length; i++) {
+  cashMoney(employees[i]);
+}
+
+function cashMoney(person) {
+
+  console.log(person.annualSalary);
+}
+
+for (let i = 0; i < employees.length; i++) {
+  goodEnough(employees[i]);
+}
+
+function goodEnough(person) {
+
+  console.log(person.reviewRating);
+}
+
+function bonusPercentageCalculator(employeeRating, employeeNumber, employeeSalary){
+const employeeBonus;
+if (employeeRating <= 2) {
+  return; employeeBonus = 0;
+}
+else if (employeeRating = 3) {
+  return; employeeBonus = 0.04;
+  
+}
+else if (employeeRating = 4) {
+  return; employeeBonus = 0.06;
+}
+
+else if (employeeRating = 5) {
+  return employeeBonus = 0.10
+  
+}
+} // end bonusPercentageCalculator
+
+
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
 // This problem is massive! Break the problem down. Use the debugger.
 // What is the fewest lines of code I can write and test to get just a little closer?
@@ -40,4 +95,4 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
